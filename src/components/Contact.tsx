cbdbd2b00de69e2a3b11a7ed6e6d69f28585dc80
FazlOmar9 <bt22csa026@iiitn.ac.in>
2025-04-04 18:49:30 +0530

    Updated resume link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -34,7 +34,7 @@ const Contact = () => {
     {
       icon: <FileText className='w-6 h-6 text-white' />,
       label: 'Resume',
-      link: 'https://drive.google.com/file/d/1Nl7sn8uodCec3HOwLhRsh1tX7UW_ZIt4/view?usp=sharing',
+      link: 'https://drive.google.com/file/d/1l-OOm9lxURf8JTOmX-X0abuaqPvcybZX/view?usp=sharing',
       position: { top: '65%', left: '45%' },
     },
   ];
